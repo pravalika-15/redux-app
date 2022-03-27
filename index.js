66c5e70439701ac0a3f8b37a1ad8398f3f866920
pravalika-15 <84198377+pravalika-15@users.noreply.github.com>
2022-03-27 16:47:08 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import { productsReducer, selectedProductsReducer } from "./productsReducer";
-const reducers = combineReducers({
-  allProducts: productsReducer,
-  product: selectedProductsReducer
-});
-export default reducers;
-
-// turns reducing functions into a single reducing function
