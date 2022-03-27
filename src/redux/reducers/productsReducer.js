ee17033153e1651e5a3e8f745764720f956e91fa
pravalika-15 <84198377+pravalika-15@users.noreply.github.com>
2022-03-27 16:41:01 +0530

    Create productsReducer.js

---
+++ src/redux/reducers/productsReducer.js
@@ -0,0 +1,29 @@
+import { ActionTypes } from "../constants/action-types";
+const intialState = {
+  products: [],
+};
+
+export const productsReducer = (state = intialState, { type, payload }) => {   // action is de structured as type and payload
+  switch (type) {
+    case ActionTypes.SET_PRODUCTS:
+      return { ...state, products: payload };
+    default:
+      return state;
+  }
+};
+
+export const selectedProductsReducer = (state = {}, { type, payload }) => {
+
+  switch (type) {
+    case ActionTypes.SELECTED_PRODUCT:
+      return { ...state, ...payload };
+    case ActionTypes.REMOVE_SELECTED_PRODUCT:
+      return {};
+    default:
+      return state;
+  }
+};
+
+
+
+
