beb5411ab1021237d517b11698c6a8e3ced71251
pravalika-15 <84198377+pravalika-15@users.noreply.github.com>
2022-03-27 16:37:06 +0530

    Create Header.js

---
+++ src/containers/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+const Header = () => {
+  return (
+    <>
+    <div className="ui fixed menu">
+      <div className="ui container center">
+       
+        <h2>Shop</h2>
+        
+      </div>
+      <div>
+      
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default Header;
